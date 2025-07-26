cde96368cedf3d1f57e44c9329e37f44f15f4750
udazzZZ <pirogova.dv96@mail.ru>
2025-07-27 02:32:46 +0300

    feat: add chart js graph

---
+++ chart-js-graph/src/App.tsx
@@ -0,0 +1,11 @@
+import { LineChart } from "./components/LineChart";
+
+function App() {
+    return (
+        <div style={{ width: "600px", margin: "50px auto" }}>
+            <LineChart />
+        </div>
+    );
+}
+
+export default App;
---
+++ chart-js-graph/src/components/LineChart.tsx
@@ -0,0 +1,52 @@
+import {
+    Chart as ChartJS,
+    LineElement,
+    PointElement,
+    LinearScale,
+    Title,
+    CategoryScale,
+    Tooltip,
+    Legend,
+    type ChartOptions,
+    type ChartData,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+    LineElement,
+    PointElement,
+    LinearScale,
+    Title,
+    CategoryScale,
+    Tooltip,
+    Legend
+);
+
+export const LineChart = () => {
+    const data: ChartData<"line", number[], string> = {
+        labels: ["Пн", "Вт", "Ср", "Чт", "Пт"],
+        datasets: [
+            {
+                label: "Количество кофе ☕",
+                data: [1, 3, 2, 5, 4],
+                borderColor: "rgb(75, 192, 192)",
+                tension: 0.3,
+            },
+        ],
+    };
+
+    const options: ChartOptions<"line"> = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top" as const,
+            },
+            title: {
+                display: true,
+                text: "График потребления кофе за неделю",
+            },
+        },
+    };
+
+    return <Line data={data} options={options} />;
+};
